e939c5d97298f821d6ad6da5dc6ae238851baf83
Mikhail Fedoseev <blackglasscat@gmail.com>
2020-12-25 14:49:44 +0300

    Added main function - write to db

--- application/app/main.py
+++ application/app/main.py
@@ -1,19 +1,39 @@
+import time
 from os import environ as env
 
+import psycopg2
 from sanic import Sanic
 from sanic.response import text
+from sanic.log import logger
 
-import psycopg2
+app = Sanic("FBK application")
+db_config = f"dbname={env['POSTGRES_DB']} user={env['POSTGRES_USER']} password={env['POSTGRES_PASSWORD']} host=postgres"
 
-conn=psycopg2.connect(f"dbname={env['POSTGRES_DB} user={env['POSTGRES_PASSWORD']}")
+connection = None
+while connection is None:
+    try:
+        connection = psycopg2.connect(db_config)
+        logger.info("Connected to DB server.")
+        connection.close()
+    except:
+        logger.info("Waiting for DB server...")
+        time.sleep(1)
 
-app = Sanic("endpoint")
+with psycopg2.connect(db_config) as connection:
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS post_data (
+                            id serial PRIMARY KEY,
+                            ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                            data text);""")
 
-@app.get("/")
+@app.post("/")
 def index(request):
+    with psycopg2.connect(db_config) as connection:
+        with connection.cursor() as cursor:
+            logger.info(request.body)
+            cursor.execute("INSERT INTO post_data (data) VALUES (%s);", (request.body.decode(),))
     return text(
-        f"{request.remote_addr} connected to {request.url_for('index')}\n"
-        f"Forwarded: {request.forwarded}\n"
+        f"Connected to {request.url_for('index')}\n"
         f"{env['POSTGRES_PASSWORD']} {env['POSTGRES_USER']} \n"
     )
 
